e0cb92c8f13ff652376cedb05e3eb108d15782af
Your Name <prottaybadhon54@gmail.com>
2021-06-03 21:52:28 +0600

    badly hungry

--- src/App.js
+++ src/App.js
@@ -2,16 +2,6 @@ import logo from './logo.svg';
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
-import Webdevelopment from './components/Webdevelopment'
-import SliderSection from './components/SliderSection'
-import WorkSection from './components/WorkSection'
-import OurService from './components/OurService'
-import AboutUs from './components/AboutUs'
-import SubService from './components/SubService'
-import Webdevelopment from './components/Webdevelopment'
-
-
-import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
